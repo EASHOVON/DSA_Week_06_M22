2063f914ae215268a9dca33e9b65e58ad6c26b97
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-12 15:21:08 +0600

    Ready for push operation

--- stack-basic.cpp
+++ stack-basic.cpp
@@ -1,5 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *Prev;
+    Node *Next;
+
+    // Constructor
+    Node(int val)
+    {
+        this->value = val;
+        this->Prev = NULL;
+        this->Next = NULL;
+    }
+};
+
+class Stack
+{
+    Node *head;
+    Node *top;
+
+public:
+    // PUSH
+    void push(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = top = newNode;
+            return;
+        }
+
+        top->Next = newNode;
+        newNode->Prev = top;
+        top = newNode;
+    }
+};
+
 int main()
 {
     return 0;
